3fbbb4f1eae8a8673f64f22d56bc501c5b1697f6
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-04-15 01:50:59 +0000

    增加注释

--- trunk/lib/autoload.php
+++ trunk/lib/autoload.php
@@ -1,72 +1,132 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +------------------------------------------------------------------------+
+// | 文件自动加载控制    					    							|
+// +------------------------------------------------------------------------+
+// | Copyright (c) 2009 Baidu. Inc. All Rights Reserved						|
+// +------------------------------------------------------------------------+
+// | Author: zhangxc <zhangxuancheng@baidu.com>								|
+// +------------------------------------------------------------------------+
+//
+// $Id$
 
 class AutoLoad
 {
 
-	private static $rules	= array();
+    /* {{{ 静态变量 */
+    /**
+     * @路径解析规则
+     */
+    private static $rules	= array();
 
-	public static function init()
-	{
-		spl_autoload_register(array(__CLASS__, 'callback'));
-	}
+    /* }}} */
 
-	public static function register($name, $dir)
-	{
-		$dir = realpath($dir);
-		if (empty($dir) || !is_dir($dir)) {
-			return false;
-		}
-		self::$rules[self::normalize($name)] = $dir;
-		return true;
-	}
+    /* {{{ public static void init() */
+    /**
+     * 初始化
+     *
+     * @access public static
+     * @return void
+     */
+    public static function init()
+    {
+        spl_autoload_register(array(__CLASS__, 'callback'));
+    }
+    /* }}} */
 
-	public static function unregister($name)
-	{
-		$name = self::normalize($name);
-		if (isset(self::$rules[$name])) {
-			unset(self::$rules[$name]);
-		}
-	}
+    /* {{{ public static void register() */
+    /*
+     * 注册路径解析规则
+     *
+     * @access public static
+     * @param  String $name
+     * @param  String $dir
+     * @return void
+     */
+    public static function register($name, $dir)
+    {
+        $dir = realpath($dir);
+        if (empty($dir) || !is_dir($dir)) {
+            return;
+        }
+        self::$rules[self::normalize($name)] = $dir;
+    }
+    /* }}} */
 
-	public static function callback($class)
-	{
-		$ordina	= $class;
-		$class	= str_replace('\\', '/', preg_replace('/[^\w\\\\]/is', '', $class));
-		$index	= strrpos($class, '/');
+    /* {{{ public static void unregister() */
+    /**
+     * 注销路径解析规则
+     *
+     * @access public static
+     * @param  String $name
+     * @return void
+     */
+    public static function unregister($name)
+    {
+        $name = self::normalize($name);
+        if (isset(self::$rules[$name])) {
+            unset(self::$rules[$name]);
+        }
+    }
+    /* }}} */
 
-		if (false === $index) {
-			throw new \Exception('dd');
-		}
+    /* {{{ public static void callback() */
+    /**
+     * 自动加载回调函数
+     *
+     * @access public static
+     * @param  String $class
+     * @return void
+     */
+    public static function callback($class)
+    {
+        $ordina	= $class;
+        $class	= str_replace('\\', '/', preg_replace('/[^\w\\\\]/is', '', $class));
+        $index	= strrpos($class, '/');
 
-		$path = strtolower(substr($class, 0, $index));
-		$name = strtolower(substr($class, $index + 1));
-		foreach (self::$rules AS $key => $dir) {
-			if (0 !== strpos($path, $key)) {
-				continue;
-			}
+        if (false === $index) {
+            throw new \Exception('dd');
+        }
 
-			$file = $dir . substr($path, strlen($key)) . '/' . $name . '.php';
-			if (is_file($file)) {
-				require $file;
-			} else {
-				throw new \Exception(sprintf('File "%s" Not Found.', $file));
-			}
+        $path = strtolower(substr($class, 0, $index));
+        $name = strtolower(substr($class, $index + 1));
+        foreach (self::$rules AS $key => $dir) {
+            if (0 !== strpos($path, $key)) {
+                continue;
+            }
 
-			if (!class_exists($ordina)) {
-				throw new \Exception(sprintf('Class "%s" Not Found in "%s".', $ordina, $file));
-			}
+            $file = $dir . substr($path, strlen($key)) . '/' . $name . '.php';
+            if (is_file($file)) {
+                require $file;
+            } else {
+                throw new \Exception(sprintf('File "%s" Not Found.', $file));
+            }
 
-			return;
-		}
+            if (!class_exists($ordina)) {
+                throw new \Exception(sprintf('Class "%s" Not Found in "%s".', $ordina, $file));
+            }
 
-		throw new \Exception(sprintf('Class "%s" Not Found.', $ordina));
-	}
+            return;
+        }
 
-	private static function normalize($name)
-	{
-		$name = preg_replace('/[^\w\\\\]/is', '', $name);
-		return strtolower(rtrim(str_replace('\\', '/', $name), '/'));
-	}
+        throw new \Exception(sprintf('Class "%s" Not Found.', $ordina));
+    }
+    /* }}} */
+
+    /* {{{ private static String normalize() */
+    /**
+     * 规则归一化处理
+     *
+     * @access private static
+     * @param  String $name
+     * @return String
+     */
+    private static function normalize($name)
+    {
+        $name = preg_replace('/[^\w\\\\]/is', '', $name);
+        return strtolower(rtrim(str_replace('\\', '/', $name), '/'));
+    }
+    /* }}} */
 
 }
 
