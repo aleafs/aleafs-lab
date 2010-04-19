57e70afa205243ceba8cf8524566d1fe7b55a2ce
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-04-19 16:01:00 +0000

    不知道为啥就是不能提交成功

--- lib/class/session.php
+++ lib/class/session.php
@@ -10,11 +10,14 @@
 //
 // $Id: session.php 2010-04-19 13:54:32 pengchun Exp $
 
-namespace Aleafs\\Lib;
+namespace Aleafs\Lib;
+
+use \Aleafs\Lib\Cookie;
 
 class Session
 {
 
+    /* {{{ 静态变量 */
     /**
      * @Session数组
      */
@@ -26,6 +29,11 @@ class Session
     private static $sign = null;
 
     /**
+     * @Cookie名字
+     */
+    private static $name = 'PHPSESSID';
+
+    /**
      * @Session ID
      */
     private static $sid = null;
@@ -35,19 +43,91 @@ class Session
      */
     private static $obj = null;
 
-    /**
-     * @Session配置
-     */
-    private static $ini = array();
+    /* }}} */
 
-    public static function start()
+    /* {{{ public static void start() */
+    /**
+     * 开始session
+     *
+     * @access public static
+     * @param  Mixture $ini (default null)
+     * @return void
+     */
+    public static function start($ini = null)
     {
-        $json = '';
-        self::$sign = crc32($json);
-        self::$data = json_decode($json, true);
+        Cookie::init(array());
+
+        self::$sid = trim(Cookie::get(self::$name));
+        if (empty(self::$sid) || !self::check($sid)) {
+            self::$sid  = self::sessid();
+            Cookie::set(self::$name, self::$sid);
+
+            self::$sign = crc32('');
+            self::$data = array();
+        } else {
+            $json = '';
+            self::$sign = crc32($json);
+            self::$data = json_decode($json, true);
+        }
+
         self::$obj = new SessionDestructor();
     }
+    /* }}} */
 
+    /* {{{ public static void set() */
+    /**
+     * 写入session
+     *
+     * @access public static
+     * @param  String $key
+     * @param  Mixture $val
+     * @param  Boolean $flush (default false)
+     * @return void
+     */
+    public static function set($key, $val, $flush = false)
+    {
+        self::$data[trim($key)] = $val;
+        if ($flush) {
+            self::flush();
+        }
+    }
+    /* }}} */
+
+    /* {{{ public static void destroy() */
+    /**
+     * 清空session
+     *
+     * @access public static
+     * @return void
+     */
+    public static function destroy()
+    {
+        self::$data = array();
+        Cookie::set(self::$name, '');
+    }
+    /* }}} */
+
+    /* {{{ public static void close() */
+    /**
+     * 关闭session
+     *
+     * @access public static
+     * @return void
+     */
+    public static function close()
+    {
+        return self::flush();
+    }
+    /* }}} */
+
+    /* {{{ public static Mixture get() */
+    /**
+     * 读session
+     *
+     * @access public static
+     * @param  String $key
+     * @return Mixture
+     */
     public static function get($key)
     {
         $key = trim($key);
@@ -57,42 +137,66 @@ class Session
 
         return self::$data[$key];
     }
+    /* }}} */
 
-    public static function set($key, $val)
-    {
-        self::$data[trim($key)] = $val;
-    }
-
-    public static function destroy()
-    {
-        self::$data = array();
-    }
-
-    public static function write()
+    /* {{{ public static Boolean flush() */
+    /**
+     * 写入session
+     *
+     * @access public static
+     * @return Boolean true or false
+     */
+    public static function flush()
     {
         $sign = crc32(json_encode(self::$data));
         if (self::$sign == $sign) {
             return true;
         }
+
+        //TODO: write to storage
         self::$sign = $sign;
 
         return true;
     }
+    /* }}} */
 
-    public static function close()
+    /* {{{ private static Boolean check() */
+    /**
+     * 校验sessid 是否合法
+     *
+     * @access private static
+     * @param  String $sid
+     * @return Boolean true or false
+     */
+    private static function check($sid)
     {
-        return self::write();
+        return true;
     }
+    /* }}} */
+
+    /* {{{ private static String sessid() */
+    /**
+     * 生成一个新的sessid
+     *
+     * @access private static
+     * @return String
+     */
+    private static function sessid()
+    {
+    }
+    /* }}} */
 
 }
 
+/* {{{ class SessionDestructor() */
 class SessionDestructor
 {
     public function __destruct()
     {
-        Session::write();
+        Session::flush();
     }
 }
+/* }}} */
 
 Session::start();
 
