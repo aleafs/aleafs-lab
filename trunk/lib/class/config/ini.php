b2b6d92c14ca1149af30768df83a6850e67ded77
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-04-19 16:02:01 +0000

    试试这次能不能提交成功

---
+++ trunk/lib/class/config/ini.php
@@ -0,0 +1,31 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +--------------------------------------------------------------------+
+// | INI配置解析类														|
+// +--------------------------------------------------------------------+
+// | Copyright (c) 2010 Taobao.com. All Rights Reserved					|
+// +--------------------------------------------------------------------+
+// | Author: pengchun <pengchun@taobao.com>								|
+// +--------------------------------------------------------------------+
+//
+// $Id$
+
+namespace Aleafs\Lib\Configer;
+
+class Ini
+{
+
+    private $url;
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function parse()
+    {
+        return parse_ini_file($this->url['path'], true);
+    }
+
+}
+
