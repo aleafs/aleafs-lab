ec69b73ed54b35259b2ae1e6e78cb78b0b2dd773
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-11-02 02:47:32 +0000

    DDL修改

--- sem/ddl.sql
+++ sem/ddl.sql
@@ -3,11 +3,11 @@ CREATE TABLE useracct (
 	userid int(10) unsigned not null auto_increment PRIMARY KEY,
 	usertype smallint(5) unsigned not null default 0,
 	userstat smallint(5) unsigned not null default 0,
-	apitoken char(32) not null default '',
 	password char(32) not null default '',
-	username varchar(128) not null default '',
+	username varchar(64) not null default '',
+	addtime datetime not null default '0000-00-00 00:00:00',
+	modtime datetime not null default '0000-00-00 00:00:00',
 	UNIQUE KEY uk_user_name (username),
-	UNIQUE KEY uk_user_token (apitoken)
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE user_permission (
@@ -24,11 +24,22 @@ CREATE TABLE user_permission (
 	KEY idx_userperm_uid (userid)
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8;
 
-INSERT INTO useracct (apitoken, username) VALUES ('wflkfwe234234', 'zhangxc83@sohu.com');
+INSERT INTO useracct (username) VALUES ('zhangxc83@sohu.com');
 INSERT INTO user_permission (pm_func, perm_se, begdate, enddate, se_name) VALUES('base', 'baidu', '2010-01-01', '2010-12-31', '百度登录名');
 
 -- 查询权限, 同时传入apitoken和se_name，避免泄露apitoken,甚至可以考虑绑定机器名
 SELECT pm_stat,begdate,enddate FROM useracct LEFT JOIN user_permission ON a.userid = b.userid WHERE apitoken = ? AND perm_se = 'baidu' AND se_name = ?
+
+-- 客户端与账户绑定表
+CREATE TABLE agent_machine (
+	agentid int(10) unsigned not null auto_increment PRIMARY KEY,
+	userid int(10) unsigned not null default 0,
+	agtstat smallint(5) unsigned not null default 0,
+	machine varchar(32) not null default '',
+	addtime datetime not null default '0000-00-00 00:00:00',
+	modtime datetime not null default '0000-00-00 00:00:00',
+	KEY idx_machine_uid (userid, machine)
+) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
 
 -- 客户端在线情况
 -- 可根据prov_name 来解决分地域排名问题
@@ -38,6 +49,7 @@ CREATE TABLE agent_session (
 	heartbeat int(10) unsigned not null default 0,
 	addtime datetime not null default '0000-00-00 00:00:00',
 	prov_name varchar(10) not null default '',
+	city_name varchar(10) not null default '',
 	ipaddr varchar(15) not null default '',
 	machine varchar(32) not null default '',
 	PRIMARY KEY pk_assess_id (sessid),
