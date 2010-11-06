4939363188a35d3fdfbb3961efc1b8d7bce45945
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-11-06 05:54:05 +0000

    增加控制器

---
+++ trunk/lib/class/controller.php
@@ -0,0 +1,69 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +------------------------------------------------------------------------+
+// | 默认控制器		    					    							|
+// +------------------------------------------------------------------------+
+// | Copygight (c) 2010 Aleafs.Com. All Rights Reserved						|
+// +------------------------------------------------------------------------+
+// | Author: zhangxc <zhangxc83@sohu.com>									|
+// +------------------------------------------------------------------------+
+//
+// $Id: autoload.php 22 2010-04-15 16:28:45Z zhangxc83 $
+
+namespace Aleafs\Lib;
+
+use \Aleafs\Lib\Exception;
+
+class Controller
+{
+
+	/* {{{ public void __construct() */
+	/**
+	 * 构造函数
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function __construct()
+	{
+	}
+	/* }}} */
+
+	/* {{{ public void execute() */
+	/**
+	 * 执行请求
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function execute($action, $param, $post = null)
+	{
+		$action	= strtolower(trim($action));
+		if (empty($action)) {
+			$action	= 'index';
+		}
+
+		$method	= sprintf('action%s', ucfirst($action));
+		if (!method_exists($this, $method)) {
+			throw new Exception(sprintf('Undefined action named as "%s"', $action));
+		}
+
+		return $this->$method($param, $post);
+	}
+	/* }}} */
+
+	/* {{{ protected void actionIndex() */
+	/**
+	 * 默认动作
+	 *
+	 * @access protected
+	 * @return void
+	 */
+	protected function actionIndex($param, $post = null)
+	{
+		echo '<!--STATUS OK-->';
+	}
+	/* }}} */
+
+}
+
