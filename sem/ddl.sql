2bcefe8915b54ac1c7f221141d1810a882ad34df
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-11-07 10:49:50 +0000

    DDL更改

--- sem/ddl.sql
+++ sem/ddl.sql
@@ -7,6 +7,7 @@ CREATE TABLE useracct (
 	username varchar(64) not null default '',
 	addtime datetime not null default '0000-00-00 00:00:00',
 	modtime datetime not null default '0000-00-00 00:00:00',
+	email varchar(200) not null default '',
 	UNIQUE KEY uk_user_name (username),
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8;
 
@@ -15,13 +16,14 @@ CREATE TABLE user_permission (
 	userid int(10) unsigned not null default 0,
 	pm_stat smallint(5) unsigned not null default 0,
 	pm_func char(10) not null default '',
-	perm_se char(10) not null default '',
+	se_name char(10) not null default '',
 	addtime datetime not null default '0000-00-00 00:00:00',
 	modtime datetime not null default '0000-00-00 00:00:00',
 	begdate date not null default '0000-00-00',
 	enddate date not null default '0000-00-00',
-	se_name varchar(128) not null default '',
-	KEY idx_userperm_uid (userid)
+	se_user varchar(128) not null default '',
+	KEY idx_userperm_uid (userid),
+	KEY idx_perm_token (se_user, pm_func, se_name)
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8;
 
 INSERT INTO useracct (username) VALUES ('zhangxc83@sohu.com');
@@ -34,29 +36,33 @@ SELECT pm_stat,begdate,enddate FROM useracct LEFT JOIN user_permission ON a.user
 CREATE TABLE agent_machine (
 	agentid int(10) unsigned not null auto_increment PRIMARY KEY,
 	userid int(10) unsigned not null default 0,
-	agtstat smallint(5) unsigned not null default 0,
-	machine varchar(32) not null default '',
+	aclstat smallint(5) unsigned not null default 0,
+	se_name char(10) not null default '',
+	machine varchar(64) not null default '',
+	nodename varchar(100) not null default '',
 	addtime datetime not null default '0000-00-00 00:00:00',
 	modtime datetime not null default '0000-00-00 00:00:00',
-	KEY idx_machine_uid (userid, machine)
+	KEY idx_machine_uid (userid, machine, se_name)
 ) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
 
 -- 客户端在线情况
 -- 可根据prov_name 来解决分地域排名问题
 CREATE TABLE agent_session (
-	sessid char(32) not null default '',
+	sessid char(32) not null default '' PRIMARY KEY,
 	userid int(10) unsigned not null default 0,
 	heartbeat int(10) unsigned not null default 0,
 	addtime datetime not null default '0000-00-00 00:00:00',
 	prov_name varchar(10) not null default '',
 	city_name varchar(10) not null default '',
 	ipaddr varchar(15) not null default '',
-	machine varchar(32) not null default '',
-	PRIMARY KEY pk_assess_id (sessid),
+	machine varchar(64) not null default '',
+	nodename varchar(100) not null default '',
 	KEY idx_agentsess_beat (heartbeat),
 	KEY idx_agentsess_prov (prov_name)
 ) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
 
+
+---- 以下为各搜索引擎使用的表
 -- Q值
 CREATE TABLE baidu_word_q (
 	keywid int(10) not null default 0,
