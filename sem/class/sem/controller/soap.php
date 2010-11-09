22aff25feecf68ad14873409597ee5ac8e0d5702
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-11-09 03:47:09 +0000

    soap_server开发更新

--- sem/class/sem/controller/soap.php
+++ sem/class/sem/controller/soap.php
@@ -14,61 +14,49 @@
 class Aleafs_Sem_Controller_Soap extends Aleafs_Lib_Controller
 {
 
-	/* {{{ 成员变量 */
+    /* {{{ 成员变量 */
 
-	private $server;
+    private $server;
 
-	/* }}} */
+    /* }}} */
 
-	/* {{{ public void __construct() */
-	/**
-	 * 构造函数
-	 *
-	 * @access public
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		$this->server	= new Aleafs_Lib_Soap_Server();
-	}
-	/* }}} */
+    /* {{{ public void __construct() */
+    /**
+     * 构造函数
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->server	= new Aleafs_Lib_Soap_Server();
+    }
+    /* }}} */
 
-	/* {{{ protected void actionIndex() */
-	/**
-	 * 默认ACTION
-	 *
-	 * @access protected
-	 * @return void
-	 */
-	protected function actionIndex($param, $post = null)
-	{
-	}
-	/* }}} */
+    /* {{{ protected void actionAccess() */
+    /**
+     * 基本服务请求
+     *
+     * @access protected
+     * @return void
+     */
+    protected function actionAccess($param, $post = null)
+    {
+    }
+    /* }}} */
 
-	/* {{{ protected void actionAccess() */
-	/**
-	 * 基本服务请求
-	 *
-	 * @access protected
-	 * @return void
-	 */
-	protected function actionAccess($param, $post = null)
-	{
-	}
-	/* }}} */
-
-	/* {{{ protected void actionBaidu() */
-	/**
-	 * 百度服务请求
-	 *
-	 * @access protected
-	 * @return void
-	 */
-	protected function actionBaidu($param, $post = null)
-	{
-	}
-	/* }}} */
+    /* {{{ protected void actionBaidu() */
+    /**
+     * 百度服务请求
+     *
+     * @access protected
+     * @return void
+     */
+    protected function actionBaidu($param, $post = null)
+    {
+    }
+    /* }}} */
 
 }
 
