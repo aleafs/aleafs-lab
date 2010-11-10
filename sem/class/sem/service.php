e67831e8b07de93a187cedabb14640617a8acc84
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-11-10 03:57:40 +0000

    SOAP header开发更新

--- sem/class/sem/service.php
+++ sem/class/sem/service.php
@@ -1,17 +1,37 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
 // +------------------------------------------------------------------------+
-// | 基本服务处理类	    					    							|
+// | SOAP服务基类	    					    							|
 // +------------------------------------------------------------------------+
 // | Copygight (c) 2010 Aleafs.Com. All Rights Reserved						|
 // +------------------------------------------------------------------------+
 // | Author: zhangxc <zhangxc83@sohu.com>									|
 // +------------------------------------------------------------------------+
 //
-// $Id: autoload.php 22 2010-04-15 16:28:45Z zhangxc83 $
+// $Id: service.php 22 2010-04-15 16:28:45Z zhangxc83 $
 //
 
-class Aleafs_Sem_Service_Access extends Aleafs_Sem_Service
+class Aleafs_Sem_Service
 {
+
+	/* {{{ 成员变量 */
+
+	protected $authenticated	= false;
+
+	/* }}} */
+
+	/* {{{ public void authenticate() */
+	/**
+	 * 用户权限验证
+	 *
+	 * @access public void
+	 * @return void
+	 */
+	public function authenticate($appname, $username, $machine, $nodename)
+	{
+		$this->authenticated	= true;
+	}
+	/* }}} */
+
 }
 
