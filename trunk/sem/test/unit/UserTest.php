fa50c8aa86e3c0d66d83e14632feb4bed3624ee1
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-11-13 06:28:41 +0000

    getAll bug fix

--- trunk/sem/test/unit/UserTest.php
+++ trunk/sem/test/unit/UserTest.php
@@ -15,6 +15,7 @@ require_once(__DIR__ . '/../../class/sem/TestShell.php');
 class Aleafs_Sem_UserTest extends Aleafs_Sem_TestShell
 {
 
+    /* {{{ protected void setUp() */
     protected function setUp()
     {
         parent::setUp();
@@ -22,15 +23,25 @@ class Aleafs_Sem_UserTest extends Aleafs_Sem_TestShell
 
         Aleafs_Sem_User::cleanPermission('unittest', 'baidu');
     }
+    /* }}} */
 
+    /* {{{ protected void tearDown() */
     protected function tearDown()
     {
         parent::tearDown();
-	}
+    }
+    /* }}} */
 
     /* {{{ public void test_should_insert_select_permission_works_fine() */
     public function test_should_insert_select_permission_works_fine()
     {
+        $this->assertTrue(Aleafs_Sem_User::addPermission('unittest', 'baidu', array(
+            'pm_stat'   => Aleafs_Sem_User::STAT_NORMAL,
+            'pm_type'   => 1,
+            'pm_func'   => 'BASE',
+            'begdate'   => 20101111,
+            'enddate'   => 20501111,
+        )));
     }
     /* }}} */
 
