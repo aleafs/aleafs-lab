a35a7160881784cc3aa9e3843450f5304049e317
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-11-13 07:07:37 +0000

    开发更新

--- sem/class/sem/service.php
+++ sem/class/sem/service.php
@@ -14,24 +14,70 @@
 class Aleafs_Sem_Service
 {
 
-	/* {{{ 成员变量 */
+    /* {{{ 静态常量 */
 
-	protected $authenticated	= false;
+    const E_SUCCESS     = 0;
+    const E_NOT_TRIAL   = 401;
+    const E_SYS_ERROR   = 501;
 
-	/* }}} */
+    /* }}} */
 
-	/* {{{ public void authenticate() */
-	/**
-	 * 用户权限验证
-	 *
-	 * @access public void
-	 * @return void
-	 */
-	public function authenticate($appname, $username, $machine, $nodename)
-	{
-		$this->authenticated	= true;
-	}
-	/* }}} */
+    /* {{{ 静态常量 */
+
+    private static $errorMessage    = array(
+        self::E_SUCCESS     => '',
+        self::E_NOT_TRIAL   => '您不是试用用户',
+        self::E_SYS_ERROR   => '系统错误',
+    );
+
+    /* }}} */
+
+    /* {{{ 成员变量 */
+
+    protected $authenticated	= false;
+
+    protected $permissions      = array();
+
+    protected $errno    = self::E_SUCCESS;
+
+    /* }}} */
+
+    /* {{{ public void authenticate() */
+    /**
+     * 用户权限验证
+     *
+     * @access public void
+     * @return void
+     */
+    public function authenticate($appname, $username, $machine, $nodename)
+    {
+        $perms  = Aleafs_Sem_User::getPermission($username, $appname);
+        if (empty($perms)) {
+            $trials = Aleafs_Lib_Configer::instance('trial_user');
+            $days   = $trials->get(sprintf('%s.%s', $appname, $username), 0);
+            if (empty($days)) {
+                $this->errno    = self::E_NOT_TRIAL;
+                return;
+            }
+
+            $pmid   = Aleafs_Sem_User::addPermission($username, $appname, array(
+                'pm_stat'   => Aleafs_Sem_User::STAT_NORMAL,
+                'pm_type'   => Aleafs_Sem_User::PERM_TRIAL,
+                'pm_func'   => 'BASE',
+                'begdate'   => date('Y-m-d'),
+                'enddate'   => date('Y-m-d', time() + 86400 * ($days - 1)),
+            ));
+            if (empty($pmid)) {
+                $this->errno    = self::E_SYS_ERROR;
+                return;
+            }
+            $perms  = Aleafs_Sem_User::getPermission($username, $appname);
+        }
+
+        $this->permissions      = $perms;
+        $this->authenticated	= true;
+    }
+    /* }}} */
 
     /* {{{ public void checkAuth() */
     /**
