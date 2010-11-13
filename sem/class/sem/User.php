ec8839ae41e16434a3c29ebb002594e993d64af9
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-11-13 03:10:05 +0000

    开发单元测试更新

---
+++ sem/class/sem/User.php
@@ -0,0 +1,84 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +------------------------------------------------------------------------+
+// | 用户类			    					    							|
+// +------------------------------------------------------------------------+
+// | Copygight (c) 2010 Aleafs.Com. All Rights Reserved						|
+// +------------------------------------------------------------------------+
+// | Author: zhangxc <zhangxc83@sohu.com>									|
+// +------------------------------------------------------------------------+
+//
+// $Id: user.php 22 2010-04-15 16:28:45Z zhangxc83 $
+
+class Aleafs_Sem_User
+{
+
+	/* {{{ 静态常量 */
+
+	const TYPE_ADMIN	= 10000;
+	const TYPE_CLIENT	= 20000;
+
+	const STAT_NORMAL	= 200;
+	const STAT_DISABLE	= 500;
+
+	/* }}} */
+
+	/* {{{ 静态变量 */
+
+	private static $loader;
+
+	/* }}} */
+
+	/* {{{ public static Mixture getPermission() */
+	/**
+	 * 获取用户权限
+	 *
+	 * @access public static
+	 * @param  String $name
+	 * @return Mixture
+	 */
+	public static function getPermission($user)
+	{
+	}
+	/* }}} */
+
+	/* {{{ public static Boolean addPermission() */
+	/**
+	 * 添加用户权限
+	 *
+	 * @access public static
+	 * @return Boolean true or false
+	 */
+	public static function addPermission($user, $perm)
+	{
+	}
+	/* }}} */
+
+	/* {{{ public static Mixture getInfoByName() */
+	/**
+	 * 根据用户名获取信息
+	 *
+	 * @access public static
+	 * @return Mixture
+	 */
+	public static function getInfoByName($name)
+	{
+	}
+	/* }}} */
+
+    /* {{{ private static void initDb() */
+    /**
+     * 初始化DB对象
+     *
+     * @access private static
+     * @return void
+     */
+    private static function initDb()
+    {
+        if (empty(self::$loader)) {
+            self::$loader   = new Aleafs_Lib_Db_Mysql('mysql');
+        }
+    }
+    /* }}} */
+
+}
---
+++ sem/test/unit/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +--------------------------------------------------------------------+
+// | UserTest.php									    				|
+// +--------------------------------------------------------------------+
+// | Copyright (c) 2010 Aleafs.com. All Rights Reserved					|
+// +--------------------------------------------------------------------+
+// | Author: aleafs <zhangxc83@sohu.com>								|
+// +--------------------------------------------------------------------+
+//
+// $Id: UserTest.php 2010-06-01 aleafs Exp $
+
+require_once(__DIR__ . '/../../class/sem/TestShell.php');
+
+class Aleafs_Sem_UserTest extends Aleafs_Sem_TestShell
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+	}
+
+    /* {{{ public void test_ll() */
+    public function test_ll()
+    {
+    }
+    /* }}} */
+
+}
