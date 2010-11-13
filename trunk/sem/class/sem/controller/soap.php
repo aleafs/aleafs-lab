3a86e732d146b7ad3d4f4cfa04e4567c10fbb297
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-11-13 16:12:23 +0000

    bug 更新

--- trunk/sem/class/sem/controller/soap.php
+++ trunk/sem/class/sem/controller/soap.php
@@ -109,7 +109,7 @@ class Aleafs_Sem_Controller_Soap extends Aleafs_Lib_Controller
      */
     private static function wsdlfile($action)
     {
-        $cache  = sprintf('%s/../../../cache/wsdl/%s', dirname(__FILE__), $action);
+        $cache  = sprintf('%s/../../../cache/wsdl/%s.xml', dirname(__FILE__), $action);
         if (!is_file($cache) || time() - filemtime($cache) > self::WSDL_EXPIRE) {
             $dr = dirname($cache);
             if (!is_dir($dr)) {
