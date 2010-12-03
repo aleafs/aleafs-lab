278e0ef7d6066a6f71ecff3891d3cc6f5c1f4738
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-12-03 07:08:35 +0000

    管理员操作控制器

---
+++ sem/class/sem/controller/admin.php
@@ -0,0 +1,65 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +------------------------------------------------------------------------+
+// | 后台管理控制器	    					    							|
+// +------------------------------------------------------------------------+
+// | Copygight (c) 2010 Aleafs.Com. All Rights Reserved						|
+// +------------------------------------------------------------------------+
+// | Author: zhangxc <zhangxc83@sohu.com>									|
+// +------------------------------------------------------------------------+
+//
+// $Id: admin.php 22 2010-04-15 16:28:45Z zhangxc83 $
+//
+
+class Aleafs_Sem_Controller_Admin extends Aleafs_Lib_Controller
+{
+
+    /* {{{ public void __construct() */
+    /**
+     * 构造函数
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        Aleafs_Lib_Session::init('session');
+    }
+    /* }}} */
+
+    /* {{{ public void execute() */
+    /**
+     * 执行请求
+     *
+     * @access public
+     * @return void
+     */
+    public function execute($action, $param, $post = null)
+    {
+        $action	= strtolower(trim($action));
+        if ('logout' != $action) {
+            // XXX
+        }
+
+        return parent::execute($action, $param, $post);
+    }
+    /* }}} */
+
+    /* {{{ protected void actionIndex() */
+    /**
+     * 默认动作
+     *
+     * @access protected
+     * @return void
+     */
+    protected function actionIndex($param, $post = null)
+    {
+        $render = new Aleafs_Lib_Render_Html();
+        $render->assign('webroot',  Aleafs_Lib_Context::get('webroot'));
+        $render->render('index', 'admin', true);
+    }
+    /* }}} */
+
+}
+
