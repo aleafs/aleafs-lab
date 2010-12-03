803e638a0c15fc4109ff5ee772e148d511f7f1b7
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-12-03 07:42:59 +0000

    开发更新，增加controller的redirect方法

--- trunk/lib52/class/controller.php
+++ trunk/lib52/class/controller.php
@@ -13,53 +13,69 @@
 class Aleafs_Lib_Controller
 {
 
-	/* {{{ public void __construct() */
-	/**
-	 * 构造函数
-	 *
-	 * @access public
-	 * @return void
-	 */
-	public function __construct()
-	{
-	}
-	/* }}} */
+    /* {{{ public void __construct() */
+    /**
+     * 构造函数
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+    /* }}} */
 
-	/* {{{ public void execute() */
-	/**
-	 * 执行请求
-	 *
-	 * @access public
-	 * @return void
-	 */
-	public function execute($action, $param, $post = null)
-	{
-		$action	= strtolower(trim($action));
-		if (empty($action)) {
-			$action	= 'index';
-		}
+    /* {{{ public void execute() */
+    /**
+     * 执行请求
+     *
+     * @access public
+     * @return void
+     */
+    public function execute($action, $param, $post = null)
+    {
+        $action	= strtolower(trim($action));
+        if (empty($action)) {
+            $action	= 'index';
+        }
 
-		$method	= sprintf('action%s', ucfirst($action));
-		if (!method_exists($this, $method)) {
-			throw new Aleafs_Lib_Exception(sprintf('Undefined action named as "%s"', $action));
-		}
+        $method	= sprintf('action%s', ucfirst($action));
+        if (!method_exists($this, $method)) {
+            throw new Aleafs_Lib_Exception(sprintf('Undefined action named as "%s"', $action));
+        }
 
-		return $this->$method($param, $post);
-	}
-	/* }}} */
+        return $this->$method($param, $post);
+    }
+    /* }}} */
 
-	/* {{{ protected void actionIndex() */
-	/**
-	 * 默认动作
-	 *
-	 * @access protected
-	 * @return void
-	 */
-	protected function actionIndex($param, $post = null)
-	{
-		echo '<!--STATUS OK-->';
-	}
-	/* }}} */
+    /* {{{ public void redirect() */
+    /**
+     * 重定向请求
+     *
+     * @access public
+     * @return void
+     */
+    public function redirect($module, $action, $param = null)
+    {
+        header(sprintf('Location: %s/%s',
+            rtrim(Aleafs_Lib_Context::get('webroot'), '/'),
+            ltrim(Aleafs_Lib_Parser_Url::build($module, $action, $param), '/')
+        ));
+    }
+    /* }}} */
+
+    /* {{{ protected void actionIndex() */
+    /**
+     * 默认动作
+     *
+     * @access protected
+     * @return void
+     */
+    protected function actionIndex($param, $post = null)
+    {
+        echo '<!--STATUS OK-->';
+    }
+    /* }}} */
 
 }
 
