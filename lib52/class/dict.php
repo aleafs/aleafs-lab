69b255109bb87af9d2ecece2125fec780e2b3c05
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2010-12-08 08:46:02 +0000

    开发更新

---
+++ lib52/class/dict.php
@@ -0,0 +1,41 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +--------------------------------------------------------------------+
+// | 字典类																|
+// +--------------------------------------------------------------------+
+// | Copyright (c) 2010 Taobao.com. All Rights Reserved					|
+// +--------------------------------------------------------------------+
+// | Author: pengchun <pengchun@taobao.com>								|
+// +--------------------------------------------------------------------+
+//
+// $Id: dict.php 47 2010-04-26 05:27:46Z zhangxc83 $
+
+class Aleafs_Lib_Dict
+{
+
+	/* {{{ 静态常量 */
+
+	const DICT_VERSION	= 1;
+
+	/* }}} */
+
+	/* {{{ 成员变量 */
+
+	private $file;
+
+	/* }}} */
+
+	/* {{{ public void __construct() */
+	/**
+	 * 构造函数
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function __construct($file, $mode = 'r')
+	{
+	}
+	/* }}} */
+
+}
+
