b787cd0acd8ec9b88b2921b92f808b9048881b28
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-01-07 10:47:19 +0000

    字典类

---
+++ lib/class/dict.php
@@ -0,0 +1,59 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +------------------------------------------------------------------------+
+// | 字典类,KEY => VALUE 本地存储结构		    							|
+// +------------------------------------------------------------------------+
+// | Copygight (c) 2010 aleafs.com. All Rights Reserved						|
+// +------------------------------------------------------------------------+
+// | Author: zhangxc <zhangxc83@sohu.com>									|
+// +------------------------------------------------------------------------+
+//
+// $Id: dict.php 22 2010-04-15 16:28:45Z zhangxc83 $
+
+namespace Aleafs\Lib;
+
+class Dict
+{
+
+	public function __construct()
+	{
+	}
+
+	public function set($key, $value)
+	{
+	}
+
+	public function get($key)
+	{
+	}
+
+	public function reset()
+	{
+	}
+
+	public function next()
+	{
+	}
+
+	public function lock()
+	{
+	}
+
+	public function optimize()
+	{
+	}
+
+	/* {{{ private Boolean writehead() */
+	/**
+	 * 写入头信息
+	 *
+	 * @access private
+	 * @return Boolean true or false
+	 */
+	private function writehead()
+	{
+	}
+	/* }}} */
+
+}
+
