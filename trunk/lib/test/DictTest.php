d4a1e8e2d6b30f1f4cd5e2c676d30adc50de822e
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-01-07 12:02:14 +0000

    Dict类开发、单元测试更新

---
+++ trunk/lib/test/DictTest.php
@@ -0,0 +1,41 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +--------------------------------------------------------------------+
+// | DictTestShell														|
+// +--------------------------------------------------------------------+
+// | Copyright (c) 2010 Taobao.com. All Rights Reserved					|
+// +--------------------------------------------------------------------+
+// | Author: pengchun <pengchun@taobao.com>								|
+// +--------------------------------------------------------------------+
+//
+// $Id: ApcTest.php 380 2011-01-07 10:18:20Z zhangxc83 $
+
+use \Aleafs\Lib\Dict;
+
+require_once(__DIR__ . '/../class/TestShell.php');
+
+class DictTest extends \Aleafs\Lib\LibTestShell
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+	}
+
+	/* {{{ public void test_should_create_dict_works_fine() */
+	public function test_should_create_dict_works_fine()
+	{
+		$file	= __DIR__ . '/temp/test_create.dict';
+		@unlink($file);
+
+		$dict	= new Dict($file);
+	}
+	/* }}} */
+
+}
+
