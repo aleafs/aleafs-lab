5db159a58b07fc9639fdacf8871a7dd21d914175
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-05-17 10:38:10 +0000

    路由读取类开发

---
+++ trunk/myfox/app/router.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Myfox\App;
+
+class Router
+{
+
+	/* {{{ 静态常量 */
+
+	const FLAG_PRE_IMPORT	= 1;	/**<	路由计算完，等待装载数据	*/
+	const FLAG_IMPORT_END	= 2;	/**<	数据装完，等待路由生效		*/
+	const FLAG_NORMAL_USE	= 3;	/**<	数据装完，路由生效			*/
+	const FLAG_PRE_RESHIP	= 4;	/**<	等待重装					*/
+	const FLAG_IS_LOCKING	= 5;	/**<	热数据迁移时使用			*/
+	const FLAG_IS_DELETED	= 0;	/**<	废弃路由，等待删除			*/
+
+	/* }}} */
+
+	/* {{{ 静态变量 */
+
+	private static $db;
+
+	/* }}} */
+
+	/* {{{ public static Mixture get() */
+	/**
+	 * 获取路由值
+	 *
+	 * @access public static
+	 * @param  String $tbname
+	 * @param  Mixture $field
+	 * @return Mixture
+	 */
+	public static function get($tbname, $field = array())
+	{
+		return self::parse(self::load(sprintf(
+			'%s:%s', $tbname, json_encode($field)
+		)));
+	}
+	/* }}} */
+
+	/* {{{ public static Integer sign() */
+	/**
+	 * 返回字符串的签名
+	 *
+	 * @access public static
+	 * @param  String $char
+	 * @return Integer
+	 */
+	public static function sign($char)
+	{
+		$sign   = 5381;
+		for ($i = 0, $len = strlen($char); $i < $len; $i++) {
+			$sign   = ($sign << 5) + $sign + ord(substr($char, $i, 1));
+		}
+
+		return sprintf('%u', $sign);
+	}
+	/* }}} */
+
+	/* {{{ private static String load() */
+	/**
+	 * 从DB中加载路由数据
+	 *
+	 * @access private static
+	 * @return String
+	 */
+	private static function load($char)
+	{
+		$ln	= self::$db->getRow(sprintf(
+			"SELECT modtime,split_info FROM %s WHERE idxsign = %u AND routes = '%s' AND useflag IN (%d, %d, %d)",
+			'',	self::sign($char), self::$db->escape($char),
+			self::FLAG_NORMAL_USE, self::FLAG_PRE_RESHIP, self::FLAG_IS_LOCKING
+		));
+
+		if (empty($ln)) {
+			return '';
+		}
+
+		return sprintf('%s|%s', $ln['modtime'], $ln['split_info']);
+	}
+	/* }}} */
+
+	/* {{{ private static Mixture parse() */
+	/**
+	 * 路由结果解析
+	 *
+	 * @access private static
+	 * @param  String $char
+	 * @return Mixture
+	 */
+	private static function parse($char)
+	{
+		list($time, $char) = array_pad(explode('|', trim($char), 2), 2, '');
+		$time	= strtotime($time);
+		$route	= array();
+		foreach (explode("\n", trim($char)) AS $ln) {
+			list($node, $name) = explode("\t", $ln);
+			$route[]	= array(
+				'time'	=> $time,
+				'node'	=> $node,
+				'name'	=> $name,
+			);
+		}
+
+		return $route;
+	}
+	/* }}} */
+
+}
