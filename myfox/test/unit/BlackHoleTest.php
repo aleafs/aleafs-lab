7682d0146f23d3f48c88dd11f4a4217dfafe3885
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-05-18 07:06:43 +0000

    autoload\blackhole单元测试通过

--- myfox/test/unit/BlackHoleTest.php
+++ myfox/test/unit/BlackHoleTest.php
@@ -1,20 +1,11 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
-// +--------------------------------------------------------------------+
-// | SessionTestShell													|
-// +--------------------------------------------------------------------+
-// | Copyright (c) 2010 Taobao.com. All Rights Reserved					|
-// +--------------------------------------------------------------------+
-// | Author: pengchun <pengchun@taobao.com>								|
-// +--------------------------------------------------------------------+
-//
-// $Id: ApcTest.php 63 2010-05-12 07:40:08Z zhangxc83 $
 
-use \Aleafs\Lib\Blackhole;
+use \Myfox\Lib\Blackhole;
 
-require_once(__DIR__ . '/../class/TestShell.php');
+require_once(__DIR__ . '/../../lib/TestShell.php');
 
-class BlackHoleTest extends \Aleafs\Lib\LibTestShell
+class BlackHoleTest extends \Myfox\Lib\TestShell
 {
 
 	public function test_should_blackhole_works_fine()
