2dc6d539731613b4a732469508410c25c0c91d7e
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-05-19 14:44:22 +0000

    后台任务类初始化

---
+++ trunk/myfox/app/tasks/import.php
@@ -0,0 +1,23 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +------------------------------------------------------------------------+
+// | Author: aleafs <pengchun@taobao.com>									|
+// +------------------------------------------------------------------------+
+//
+// $Id: import.php 18 2010-04-13 15:40:37Z zhangxc83 $
+//
+
+namespace Myfox\App\Tasks;
+
+class Import extends \Myfox\App\Task
+{
+
+	public function execute()
+	{
+	}
+
+    public function wait()
+    {
+    }
+
+}
