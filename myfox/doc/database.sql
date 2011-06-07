238bb4d9b4d3ebe84f8adda6efb463f52d9d7e62
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-06-07 06:53:01 +0000

    表结构补充

--- myfox/doc/database.sql
+++ myfox/doc/database.sql
@@ -1,4 +1,50 @@
 
+-- 节点表
+DROP TABLE IF EXISTS node_list;
+CREATE TABLE node_list (
+	node_id smallint(5) unsigned not null auto_increment,
+	node_type tinyint(2) unsigned not null default 0,
+	node_name char(16) not null default '',
+	addtime datetime not null default '0000-00-00 00:00:00',
+	modtime datetime not null default '0000-00-00 00:00:00',
+	PRIMARY KEY pk_node_id (node_id),
+	UNIQUE KEY uk_node_name (node_name)
+) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
+
+-- 机器表
+DROP TABLE IF EXISTS host_list;
+CREATE TABLE host_list (
+	host_id int(10) unsigned not null auto_increment,
+	node_id smallint(5) unsigned not null default 0,
+	host_type tinyint(2) unsigned not null default 0,
+	host_stat tinyint(2) unsigned not null default 0,
+	host_name char(16) not null default '',
+	addtime datetime not null default '0000-00-00 00:00:00',
+	modtime datetime not null default '0000-00-00 00:00:00',
+	conn_host varchar(64) not null default '',
+	conn_port smallint(5) unsigned not null default 0,
+	user_rw varchar(128) not null default '',
+	user_ro varchar(128) not null default '',
+	PRIMARY KEY pk_host_id (host_id),
+	UNIQUE KEY uk_host_name (host_name),
+	KEY idx_host_node (node_id,host_stat,host_type)
+) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
+
+-- 配置表
+DROP TABLE IF EXISTS table_list;
+CREATE TABLE table_list (
+	autokid int(10) unsigned not null auto_increment,
+	addtime datetime not null default '0000-00-00 00:00:00',
+	modtime datetime not null default '0000-00-00 00:00:00',
+	backups tinyint(2) unsigned not null default 1,
+	loadtype tinyint(2) unsigned not null default 0,
+	tabname varchar(64) not null default '',
+	tabdesc varchar(128) not null default '',
+	filemd5 varchar(32) not null default '',
+	PRIMARY KEY pk_table_id (autokid),
+	UNIQUE KEY uk_table_name (tabname)
+) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
+
 -- 路由表
 DROP TABLE IF EXISTS route_info;
 CREATE TABLE IF NOT EXISTS route_info (
@@ -20,8 +66,14 @@ CREATE TABLE IF NOT EXISTS route_info (
 
 -- 系统状态表
 DROP TABLE IF EXISTS settings;
-CREATE TABLE IF NOT EXISTS settings ()
-ENGINE = MyISAM DEFAULT CHARSET=UTF8;
+CREATE TABLE IF NOT EXISTS settings (
+	cfgname char(64) not null default '',
+	cfgdesc varchar(255) not null default '',
+	cfgvalue varchar(255) not null default '',
+	addtime datetime not null default '0000-00-00 00:00:00',
+	modtime datetime not null default '0000-00-00 00:00:00',
+	PRIMARY KEY pk_setting_name (cfgname)
+) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
 
 -- 任务队列表
 DROP TABLE IF EXISTS task_queque;
@@ -45,5 +97,4 @@ CREATE TABLE IF NOT EXISTS task_queque (
 ) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
 
 -- SELECT ... FROM task_queque WHERE task_flag = ? AND trytimes < ? ORDER BY priority ASC, trytimes ASC, autokid ASC-- SELECT ... FROM task_queque WHERE addtime < ?
-
 
