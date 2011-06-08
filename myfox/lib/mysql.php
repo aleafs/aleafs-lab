40e2dba5872fde6d1fd6539b460afa64d1ddc808
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-06-08 03:56:48 +0000

    mysql driver update

---
+++ myfox/lib/mysql.php
@@ -0,0 +1,170 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +------------------------------------------------------------------------+
+// | MySQL操作类	    					    							|
+// |                                                                        | 
+// | Based on php5.3.3, with mysqli & mysqlnd                               |
+// +------------------------------------------------------------------------+
+// | Author: aleafs <pengchun@taobao.com>	    							|
+// +------------------------------------------------------------------------+
+//
+// $Id: mysql.php 22 2010-04-15 16:28:45Z zhangxc83 $
+
+namespace Myfox\Lib;
+
+use \Myfox\Lib\LiveBox;
+
+class Mysql
+{
+
+    /* {{{ 静态变量 */
+
+    private static $objects	= array();
+
+    private static $alias	= array();
+
+    /* }}} */
+
+    /* {{{ */
+
+    private $master;
+
+    private $slave;
+
+    /* }}} */
+
+    /* {{{ public static Object instance() */
+    /**
+     * 获取实例
+     *
+     * @access public static
+     * @return object
+     */
+    public static function instance($name)
+    {
+        $name	= self::normalize($name);
+        if (empty(self::$objects[$name])) {
+            if (empty(self::$alias[$name])) {
+                throw new \Myfox\Lib\Exception(sprintf('Undefined mysql instance named as "%s"', $name));
+            }
+            self::$objects[$name]	= new self(self::$alias[$name]);
+        }
+
+        return self::$objects[$name];
+    }
+    /* }}} */
+
+    /* {{{ public static void register() */
+    /**
+     * 注册别名
+     *
+     * @access public static
+     * @return void
+     */
+    public static function register($name, $config = null)
+    {
+        self::$alias[self::normalize($name)] = $config;
+    }
+    /* }}} */
+
+    /* {{{ public static void removeAllNames() */
+    /**
+     * 清理所有对象
+     *
+     * @access public static
+     * @return void
+     */
+    public static function removeAllNames()
+    {
+        foreach (self::$objects AS $mysql) {
+            $mysql->close();
+        }
+
+        self::$objects	= array();
+        self::$alias	= array();
+    }
+    /* }}} */
+
+    /* {{{ public void __construct() */
+    /**
+     * 构造函数
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct($config = null, $name = null)
+    {
+        if (!empty($name)) {
+            self::$objects[self::normalize($name)]  = &$this;
+        }
+    }
+    /* }}} */
+
+    /* {{{ public void __destruct() */
+    /**
+     * 
+     * @access public
+     * @return void 
+     */
+    public function __destruct()
+    {
+        $this->close();
+    }
+    /* }}} */
+
+    /* {{{ public void close() */
+    /**
+     * 
+     */
+    public function close()
+    {
+    }
+    /* }}} */
+
+    /* {{{ public Mixture query() */
+    /**
+     *
+     * @access public
+     * @return Mixture
+     */
+    public function query($query, $value = null, $type = null)
+    {
+    }
+    /* }}} */
+
+    /* {{{ public Mixture async() */
+    /**
+     * 异步请求
+     *
+     * @access public
+     * @return void
+     */
+    public function async($query, $value = null, $type = null)
+    {
+    }
+    /* }}} */
+
+    /* {{{ public Mixture poll() */
+    /**
+     * @access public
+     * @return Mixture
+     */
+    public function poll()
+    {
+    }
+    /* }}} */
+
+    /* {{{ private static string normalize() */
+    /**
+     * 名字归一化
+     *
+     * @access private static
+     * @return string
+     */
+    private static function normalize($name)
+    {
+        return strtolower(preg_replace('/\s+/', '', $name));
+    }
+    /* }}} */
+
+}
---
+++ myfox/test/unit/MysqlTest.php
@@ -0,0 +1,44 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+use \Myfox\Lib\Mysql;
+
+require_once(__DIR__ . '/../../lib/TestShell.php');
+
+class MysqlTest extends \Myfox\Lib\TestShell
+{
+
+	protected function setUp()
+	{
+		parent::setUp();
+	}
+
+	protected function tearDown()
+	{
+		Mysql::removeAllNames();
+		parent::tearDown();
+	}
+
+	/* {{{ public void test_should_mysql_factory_works_fine() */
+	public function test_should_mysql_factory_works_fine()
+	{
+		try {
+			$mysql	= Mysql::instance('i_am_not_exists');
+			$this->assertTrue(false);
+		} catch (\Exception $e) {
+			$this->assertTrue($e instanceof \Myfox\Lib\Exception);
+			$this->assertContains('Undefined mysql instance named as "i_am_not_exists"', $e->getMessage());
+		}
+
+		Mysql::register('mysql1', 'aa');
+		Mysql::register('mysql2', 'bb');
+
+		$mysql	= new Mysql('aa', 'mysql3');
+
+		$this->assertEquals(Mysql::instance('MYSQl1'), $mysql);
+		$this->assertEquals(Mysql::instance('MYSQl3'), $mysql);
+	}
+	/* }}} */
+
+}
+
