9854d5e03426b17f53acca7bcd5f031db384a3e4
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-06-09 14:32:34 +0000

    table类开发更新

--- myfox/app/model/table.php
+++ myfox/app/model/table.php
@@ -11,61 +11,89 @@ namespace Myfox\App\Model;
 class Table
 {
 
-	/* {{{ 静态变量 */
+    /* {{{ 静态变量 */
 
-	private static $objects	= array();
+    private static $objects	= array();
 
-	/* }}} */
+    /* }}} */
 
-	/* {{{ 成员变量 */
+    /* {{{ 成员变量 */
 
-	private $option	= array();
+    public $queries = 0;
 
-	/* }}} */
+    private $option	= null;
 
-	/* {{{ public static Object instance() */
-	/**
-	 * 获取表的实例
-	 *
-	 * @access public static
-	 * @param  String $tbname
-	 * @return Object
-	 */
-	public static function instance($tbname)
-	{
-		$tbname	= strtolower(trim($tbname));
-		if (empty(self::$objects[$tbname])) {
-			self::$objects[$tbname]	= new self($tbname);
-		}
+    /* }}} */
 
-		return self::$objects[$tbname];
-	}
-	/* }}} */
+    /* {{{ public static Object instance() */
+    /**
+     * 获取表的实例
+     *
+     * @access public static
+     * @param  String $tbname
+     * @return Object
+     */
+    public static function instance($tbname)
+    {
+        $tbname	= strtolower(trim($tbname));
+        if (empty(self::$objects[$tbname])) {
+            self::$objects[$tbname]	= new self($tbname);
+        }
 
-	/* {{{ public Mixture get() */
-	/**
-	 * 返回属性
-	 *
-	 * @access public
-	 * @return Mixture
-	 */
-	public function get($key, $default = null)
-	{
-		$key	= strtolower(trim($key));
-		return isset($this->option[$key]) ? $this->option[$key] : $default;
-	}
-	/* }}} */
+        return self::$objects[$tbname];
+    }
+    /* }}} */
 
-	/* {{{ private void __construct() */
-	/**
-	 * 构造函数
-	 *
-	 * @access private
-	 * @return void
-	 */
-	private function __construct($tbname)
-	{
-	}
-	/* }}} */
+    /* {{{ public static void cleanAllStatic() */
+    /**
+     * 清理静态数据
+     *
+     * @access public static
+     * @return void
+     */
+    public static function cleanAllStatic()
+    {
+        self::$objects  = array();
+    }
+    /* }}} */
+
+    /* {{{ public Mixture get() */
+    /**
+     * 返回属性
+     *
+     * @access public
+     * @return Mixture
+     */
+    public function get($key, $default = null)
+    {
+        if (null === $this->option) {
+            $mysql  = \Myfox\Lib\Mysql::instance('default');
+            $info   = $mysql->getRow($mysql->query(sprintf(
+                "SELECT * FROM %s.%stable_list WHERE tabname = '%s'",
+                $mysql->option('dbname', 'meta_myfox_config'),
+                $mysql->option('prefix', ''),
+                $mysql->escape($this->tbname)
+            )));
+            $this->option   = (array)$info;
+            $this->queries++;
+        }
+        $key	= strtolower(trim($key));
+        return isset($this->option[$key]) ? $this->option[$key] : $default;
+    }
+    /* }}} */
+
+    /* {{{ private void __construct() */
+    /**
+     * 构造函数
+     *
+     * @access private
+     * @return void
+     */
+    private function __construct($tbname)
+    {
+        $this->tbname   = strtolower(trim($tbname));
+        $this->option   = null;
+    }
+    /* }}} */
 
 }
---
+++ myfox/test/unit/TableTest.php
@@ -0,0 +1,64 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+use \Myfox\App\Model\Table;
+
+require_once(__DIR__ . '/../../lib/TestShell.php');
+
+class TableTest extends \Myfox\Lib\TestShell
+{
+
+    private static $mysql;
+
+    /* {{{ protected void setUp() */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        \Myfox\Lib\Mysql::register('default', __DIR__ . '/ini/mysql.ini');
+        self::$mysql    = \Myfox\Lib\Mysql::instance('default');
+
+        self::$mysql->query(sprintf(
+            "REPLACE INTO %s.%stable_list (addtime,modtime,loadtype,tabname, split_threshold,split_drift,route_method,route_fields) VALUES (NOW(),NOW(),1,'mirror',1000,0.2,0,''), (NOW(),NOW(),0,'numsplit',1000,0.2,1,'thedate:date')",
+            self::$mysql->option('dbname', 'meta_myfox_config'),
+            self::$mysql->option('prefix', '')
+        ));
+        Table::cleanAllStatic();
+    }
+    /* }}} */
+
+    /* {{{ protected void tearDown() */
+    protected function tearDown()
+    {
+        Table::cleanAllStatic();
+        parent::tearDown();
+    }
+    /* }}} */
+
+    /* {{{ public void test_should_get_table_option_works_fine() */
+    public function test_should_get_table_option_works_fine()
+    {
+        $table  = Table::instance('numsplit');
+        $this->assertEquals(0, $table->queries);
+
+        $this->assertEquals(1000, $table->get('split_threshold'));
+        $this->assertEquals(1, $table->queries);
+
+        $this->assertEquals(0.2, $table->get('split_drift'));
+        $this->assertEquals(1, $table->queries);
+
+        $this->assertEquals('thedate:date', $table->get('route_fields'));
+        $this->assertEquals(1, $table->queries);
+
+        $table  = Table::instance('i am not exists');
+        $this->assertEquals(0, $table->queries);
+
+        $this->assertEquals(null, $table->get('split_threshold'));
+        $this->assertEquals(1, $table->queries);
+
+        $this->assertEquals(null, $table->get('route_method'));
+        $this->assertEquals(1, $table->queries);
+    }
+    /* }}} */
+
+}
