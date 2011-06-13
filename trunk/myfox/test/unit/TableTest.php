b0f02560499e7bcf7f02c24f113fa52d94072258
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-06-13 15:20:21 +0000

    单元测试bug fix

--- trunk/myfox/test/unit/TableTest.php
+++ trunk/myfox/test/unit/TableTest.php
@@ -19,7 +19,7 @@ class TableTest extends \Myfox\Lib\TestShell
         self::$mysql    = \Myfox\Lib\Mysql::instance('default');
 
         self::$mysql->query(sprintf(
-            "REPLACE INTO %s.%stable_list (addtime,modtime,loadtype,tabname, split_threshold,split_drift,route_method,route_fields) VALUES (NOW(),NOW(),1,'mirror',1000,0.2,0,''), (NOW(),NOW(),0,'numsplit',1000,0.2,1,'thedate:date')",
+            "REPLACE INTO %s.%stable_list (addtime,modtime,loadtype,tabname, split_threshold,split_drift,route_method,route_fields) VALUES (NOW(),NOW(),1,'mirror',1000,0.2,0,''), (NOW(),NOW(),0,'numsplit',1000,0.2,1,'thedate:date,cid')",
             self::$mysql->option('dbname', 'meta_myfox_config'),
             self::$mysql->option('prefix', '')
         ));
@@ -47,7 +47,7 @@ class TableTest extends \Myfox\Lib\TestShell
         $this->assertEquals(0.2, $table->get('split_drift'));
         $this->assertEquals(1, $table->queries);
 
-        $this->assertEquals('thedate:date', $table->get('route_fields'));
+        $this->assertEquals('thedate:date,cid', $table->get('route_fields'));
         $this->assertEquals(1, $table->queries);
 
         $table  = Table::instance('i am not exists');
