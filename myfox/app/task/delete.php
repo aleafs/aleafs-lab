13ab9cf62e8ed80b4b031b00777bece3f478c2b5
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2011-06-15 10:38:09 +0000

    Task类单元测试

---
+++ myfox/app/task/delete.php
@@ -0,0 +1,23 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+// +------------------------------------------------------------------------+
+// | Author: aleafs <pengchun@taobao.com>									|
+// +------------------------------------------------------------------------+
+//
+// $Id: delete.php 18 2010-04-13 15:40:37Z zhangxc83 $
+//
+
+namespace Myfox\App\Task;
+
+class Delete extends \Myfox\App\Task
+{
+
+    public function execute()
+    {
+    }
+
+    public function wait()
+    {
+    }
+
+}
