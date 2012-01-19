0dd55e4300b4babc013007fab805442555f41478
zhangxc83 <zhangxc83@50eb06ae-2abd-11df-917d-c9ccfb47d9d5>
2012-01-19 09:58:29 +0000

    表结构更新

--- myfox/doc/database.sql
+++ myfox/doc/database.sql
@@ -1,4 +1,17 @@
 -- xxx
+-- 系统状态表
+DROP TABLE IF EXISTS test_settings;
+CREATE TABLE IF NOT EXISTS test_settings (
+	autokid int(10) unsigned not null auto_increment,
+	cfgname varchar(32) not null default '',
+	ownname varchar(32) not null default '',
+	cfgvalue varchar(255) not null default '',
+	addtime datetime not null default '0000-00-00 00:00:00',
+	modtime datetime not null default '0000-00-00 00:00:00',
+	PRIMARY KEY pk_setting_id (autokid),
+	UNIQUE KEY uk_setting_name (ownname,cfgname)
+) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
+
 -- 机器表
 DROP TABLE IF EXISTS test_host_list;
 CREATE TABLE test_host_list (
@@ -35,6 +48,7 @@ CREATE TABLE test_table_list (
 	addtime datetime not null default '0000-00-00 00:00:00',
 	modtime datetime not null default '0000-00-00 00:00:00',
 	backups tinyint(2) unsigned not null default 1,
+	max_index_num tinyint(2) unsigned not null default 0,
 	split_threshold int(10) unsigned not null default 0,
 	split_drift decimal(5,2) unsigned not null default 0.00,
 	load_type tinyint(2) unsigned not null default 0,
@@ -43,6 +57,7 @@ CREATE TABLE test_table_list (
 	table_desc varchar(128) not null default '',
 	unique_key varchar(256) not null default '',
 	xml_filemd varchar(32) not null default '',
+	sql_import text not null default '',
 	PRIMARY KEY pk_table_id (autokid),
 	UNIQUE KEY uk_table_name (table_name)
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8;
@@ -62,50 +77,41 @@ CREATE TABLE test_table_route (
 	UNIQUE KEY uk_table_column (table_name, column_name)
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8;
 
+-- 表字段配置表
 DROP TABLE IF EXISTS test_table_column;
 CREATE TABLE test_table_column (
 	autokid int(10) unsigned not null auto_increment,
-	colseqn smallint(5) unsigned not null default 0,
-	isextra tinyint(2) unsigned not null default 0,
-	tabname varchar(64) not null default '',
-	colname varchar(64) not null default '',
-	excepts varchar(100) not null default '',
-	coltype varchar(15) not null default '',
-	dfltval varchar(100),
-	coldesc varchar(100) not null default '',
-	sqlchar varchar(200) not null default '',
-	addtime datetime not null default '0000-00-00 00:00:00',
-	modtime datetime not null default '0000-00-00 00:00:00',
+	column_order smallint(5) unsigned not null default 0,
+	addtime int(10) unsigned not null default 0,
+	modtime int(10) unsigned not null default 0,
+	table_name varchar(64) not null default '',
+	column_name varchar(64) not null default '',
+	column_extra varchar(256) not null default '',
+	column_type varchar(64) not null default '',
+	column_size varchar(64) not null default '',
+	default_value varchar(64) not null default '',
+	column_desc varchar(256) not null default '',
 	PRIMARY KEY pk_column_id (autokid),
-	UNIQUE KEY uk_column_name (tabname,colname,excepts),
-	KEY idx_column_order (tabname,colseqn)
+	UNIQUE KEY uk_column_name (table_name,column_name,column_extra),
+	KEY idx_column_order (table_name(10), column_order)
 ) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
-
-INSERT INTO test_table_column (colseqn,tabname,colname,coltype,dfltval,coldesc,sqlchar,addtime,modtime) VALUES (1,'numsplit','thedate','date','0000-00-00','数据日期',"thedate date not null default '0000-00-00'", NOW(), NOW());
-INSERT INTO test_table_column (colseqn,tabname,colname,coltype,dfltval,coldesc,sqlchar,addtime,modtime) VALUES (2,'numsplit','cid','uint','0','类目ID',"cid int(10) unsigned not null default 0", NOW(), NOW());
-INSERT INTO test_table_column (colseqn,tabname,colname,coltype,dfltval,coldesc,sqlchar,addtime,modtime) VALUES (3,'numsplit','num1','uint','0','整数',"num1 int(10) unsigned not null default 0", NOW(), NOW());
-INSERT INTO test_table_column (colseqn,tabname,colname,coltype,dfltval,coldesc,sqlchar,addtime,modtime) VALUES (4,'numsplit','num2','float','0.00','浮点数',"num2 decimal(20,14) not null default 0.00", NOW(), NOW());
-INSERT INTO test_table_column (colseqn,tabname,colname,coltype,dfltval,coldesc,sqlchar,addtime,modtime) VALUES (5,'numsplit','char1','char','','字符串',"char1 varchar(32) not null default ''", NOW(), NOW());
-INSERT INTO test_table_column (colseqn,tabname,colname,coltype,dfltval,coldesc,sqlchar,addtime,modtime,isextra) VALUES (100,'numsplit','autokid','uint','0','自增列',"autokid int(10) unsigned not null auto_increment", NOW(), NOW(), 1);
+-- auto_increment字段采用单独的column_type
 
 DROP TABLE IF EXISTS test_table_index;
 CREATE TABLE test_table_index (
 	autokid int(10) unsigned not null auto_increment,
-	idxseqn smallint(5) unsigned not null default 0,
-	idxtype varchar(32) not null default '',
-	tabname varchar(64) not null default '',
-	idxname varchar(64) not null default '',
-	excepts varchar(100) not null default '',
-	idxchar varchar(1000) not null default '',
-	addtime datetime not null default '0000-00-00 00:00:00',
-	modtime datetime not null default '0000-00-00 00:00:00',
+	addtime int(10) unsigned not null default 0,
+	modtime int(10) unsigned not null default 0,
+	create_type tinyint(2) unsigned not null default 0,
+	table_name varchar(64) not null default '',
+	index_name varchar(64) not null default '',
+	index_extra varchar(256) not null default '',
+	index_text varchar(1024) not null default '',
 	PRIMARY KEY pk_index_id (autokid),
-	UNIQUE KEY uk_index_name (tabname,idxname,excepts),
-	KEY idx_index_order (tabname,idxseqn)
+	UNIQUE KEY uk_table_index (table_name, index_name, index_extra)
 ) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
-INSERT INTO test_table_index (idxseqn,idxtype,tabname,idxname,idxchar,addtime,modtime) VALUES (1, '', 'numsplit','idx_split_cid','cid', NOW(), NOW());
-INSERT INTO test_table_index (idxseqn,idxtype,tabname,idxname,idxchar,addtime,modtime) VALUES (2, 'PRIMARY', 'numsplit','pk_split_id','autokid', NOW(), NOW());
 
+-- xxx:以下还没review
 -- 路由表
 DROP TABLE IF EXISTS test_route_info;
 CREATE TABLE test_route_info (
@@ -123,19 +129,6 @@ CREATE TABLE test_route_info (
 	PRIMARY KEY pk_route_id (autokid),
 	KEY idx_route_sign (idxsign, useflag)
 ) ENGINE = InnoDB DEFAULT CHARSET=UTF8;
-
--- 系统状态表
-DROP TABLE IF EXISTS test_settings;
-CREATE TABLE IF NOT EXISTS test_settings (
-	autokid int(10) unsigned not null auto_increment,
-	cfgname varchar(32) not null default '',
-	ownname varchar(32) not null default '',
-	cfgvalue varchar(255) not null default '',
-	addtime datetime not null default '0000-00-00 00:00:00',
-	modtime datetime not null default '0000-00-00 00:00:00',
-	PRIMARY KEY pk_setting_id (autokid),
-	UNIQUE KEY uk_setting_name (ownname,cfgname)
-) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
 
 -- 任务队列表
 -- namespace支持
@@ -160,17 +153,3 @@ CREATE TABLE IF NOT EXISTS test_task_queque (
 	KEY idx_queque_time (addtime)
 ) ENGINE = MyISAM DEFAULT CHARSET=UTF8;
 
--- 准入权限表
-DROP TABLE IF EXISTS test_auth_list;
-CREATE TABLE IF NOT EXISTS test_auth_list (
-	autokid int(10) unsigned not null auto_increment,
-	appname varchar(64) not null default '',
-	actname varchar(64) not null default '',
-	ipaddr varchar(20) not null default '',
-	authvalue int(10) unsigned not null default 0,
-	addtime datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-	modtime datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-	remark varchar(100) not null default '',
-	PRIMARY KEY pk_user_id (autokid),
-	UNIQUE KEY uk_auth_token (appname, actname)
-)ENGINE = MyISAM DEFAULT CHARSET=UTF8;
